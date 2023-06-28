6c5e6ea5ba17a96493963e9e29c3777075e5dac2
Vladimir Siman <online@clew.cz>
2023-06-28 13:17:30 +0200

    package

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
-package main
+package average
 
 import "fmt"
 
-func main() {
-	cenyProdaneho := [3]float64{71.8, 56.2, 89.5}
-	var sum float64 = 0
-
-	for _, val := range cenyProdaneho {
+func GetAverage(prices ...float64) float64 {
+	var sum float64 = 0.0
+	for _, val := range prices {
 		sum += val
 	}
-	sum /= float64(len(cenyProdaneho))
 
-	fmt.Printf("Prumerna cena byla %v\n", sum)
+	sum /= float64(len(prices))
+	return sum
 }
